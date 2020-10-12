b4d29d2ca1a4ee69d5db4492ea391360f354c869
Willem Leuverink <gwleuverink@Willems-MacBook-Pro.local>
2020-10-12 20:14:14 +0200

    set up menubar scaffolding

--- src/background.js
+++ src/background.js
@@ -3,6 +3,8 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { menubar } from 'menubar'
+import { join } from 'path'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -14,32 +16,7 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
-    }
-  })
 
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
-    if (!process.env.IS_TEST) win.webContents.openDevTools()
-  } else {
-    createProtocol('app')
-    // Load the index.html when not in development
-    win.loadURL('app://./index.html')
-  }
-
-  win.on('closed', () => {
-    win = null
-  })
-}
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -50,13 +27,6 @@ app.on('window-all-closed', () => {
   }
 })
 
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
-})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -70,7 +40,30 @@ app.on('ready', async () => {
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
-  createWindow()
+
+  if (! process.env.WEBPACK_DEV_SERVER_URL) {
+    createProtocol('app')
+  }
+
+  const mb = menubar({
+      index: process.env.WEBPACK_DEV_SERVER_URL
+        ? process.env.WEBPACK_DEV_SERVER_URL
+        : 'app://./index.html',
+      browserWindow: {
+          alwaysOnTop: true,
+          minWidth: 360,
+          maxWidth: 360
+      },
+      icon: join(__static, '../src/assets/MenubarIcon.png'),
+  })
+
+  // Open the menubar window & devtools in development mode
+  mb.on('ready', () => {
+    if(!isDevelopment) return
+
+    mb.showWindow()
+    mb.window.openDevTools()
+  })
 })
 
 // Exit cleanly on request from parent process in development mode.
